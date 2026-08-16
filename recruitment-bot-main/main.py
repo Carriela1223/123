5176eb046a6824b33236ba745cff0a064f5e82f8
Carriela <bumjin1223@gmail.com>
2026-08-16 13:49:32 +0900

    Implement keep-alive HTTP server in main.py
    Added a simple HTTP server to respond with a keep-alive message.

--- recruitment-bot-main/main.py
+++ recruitment-bot-main/main.py
@@ -1,3 +1,22 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+
+class KeepAliveHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write("Bot is alive!".encode('utf-8'))
+    def log_message(self, format, *args):
+        return
+
+def run_server():
+    server = HTTPServer(('0.0.0.0', 8080), KeepAliveHandler)
+    server.serve_forever()
+
+# 백그라운드 스레드로 간이 웹 서버 기동
+threading.Thread(target=run_server, daemon=True).start()
+
 import discord
 import os
 from dotenv import load_dotenv
